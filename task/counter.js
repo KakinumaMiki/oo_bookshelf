9d09c0f3a0dbd315eb3ee856ac806affd7425914
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-26 14:17:14 +0900

    Counterクラスに以下の機能を追加 ・down: 呼ばれるとカウントが一つ減る ・resetValue: 呼ばれると今カウントしている内容が0にリセットされる

--- task/counter.js
+++ task/counter.js
@@ -1,4 +1,7 @@
-// a. 以下のような仕様のCounterクラスを作成しましょう。
+// b. 作成したCounterクラスに以下の機能を追加してください。
+
+// - down: 呼ばれるとカウントが一つ減る
+// - resetValue: 呼ばれると今カウントしている内容が0にリセットされる
 class Counter {
   constructor() {
     this.count = 0;
@@ -9,6 +12,16 @@ class Counter {
     this.count++;
   }
 
+  // countを1さげる
+  down() {
+    this.count--;
+  }
+
+  // countが0にリセットされる
+  resetValue() {
+    this.count = 0;
+  }
+ 
   // countを表示
   getValue() {
     return this.count;
@@ -20,3 +33,7 @@ counter.up();
 console.log(counter.getValue()); // => 1と表示される
 counter.up();
 console.log(counter.getValue()); // => 2と表示される
+counter.down();
+console.log(counter.getValue()); // => 1と表示される
+counter.resetValue();
+console.log(counter.getValue()); // => 0と表示される
