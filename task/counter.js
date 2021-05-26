f89e7eae705df0d2d124df47d2f098e34049c05c
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-26 14:46:51 +0900

    EvenCounterクラスの作成

--- task/counter.js
+++ task/counter.js
@@ -1,5 +1,3 @@
-// Counterクラスのインスタンスを2つ作成して、
-// それぞれのカウントが無関係に増減することを確認できるサンプルコードを書いてください。
 class Counter {
   constructor() {
     this.count = 0;
@@ -19,7 +17,7 @@ class Counter {
   resetValue() {
     this.count = 0;
   }
- 
+
   // countを表示
   getValue() {
     return this.count;
@@ -31,8 +29,38 @@ let counter2 = new Counter;
 counter1.up();
 counter1.up();
 console.log(counter1.getValue()); // => 2と表示される
-
 counter2.down();
 console.log(counter2.getValue()); // => -1と表示される
 counter2.resetValue();
 console.log(counter2.getValue()); // => 0と表示される
+
+
+class EvenCounter {
+  constructor() {
+    this.count = 0;
+    this.i = 0;
+  }
+  // 偶数回呼ばれた時countを1あげる
+  up() {
+    this.i++;
+    if (this.i % 2 === 0) {
+      this.count++;
+    }
+  }
+
+  // countを表示
+  getValue() {
+    return this.count;
+  }
+}
+
+let counter = new EvenCounter;
+
+console.log('EvenCounter');
+counter.up(); // => ここではアップしない
+counter.up(); // => ここでアップ
+console.log(counter.getValue()); // => 1と表示される
+counter.up(); // => ここではアップしない
+counter.up(); // => ここでアップ
+counter.up(); // => ここではアップしない
+console.log(counter.getValue()); // => 2と表示される
