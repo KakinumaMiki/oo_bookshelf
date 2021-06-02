040a49f690b8e6076c7730c0161c1b2f6bd0f032
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-06-02 19:13:41 +0900

    レビュー指摘対応

--- task/encapsulation_pointCalculator.js
+++ task/encapsulation_pointCalculator.js
@@ -1,4 +1,4 @@
-class ImmutablePoint {
+class Point {
   #name
   #point
 
@@ -17,31 +17,32 @@ class ImmutablePoint {
 }
 
 class PointCalculator {
-  #results
+  #points
 
+  constructor() {
+    this.#points = [];
+  }
+  
   static valueOf(arrayOfHash) {
     let pointCalculator = new this;
 
     for (let i = 0; i < arrayOfHash.length; i++) {
       let hash = arrayOfHash[i];
-      let results = new ImmutablePoint(hash.name, hash.point);
-     pointCalculator.addResult(results);
+      let points = new Point(hash.name, hash.point);
+     pointCalculator.addResult(points);
     }
     return pointCalculator;
   }
 
-  constructor() {
-    this.#results = [];
+  addResult(result) {
+    this.#points.push(result);
   }
   
-  addResult(result) {
-    this.#results.push(result);
-  }
   // pointの合計
   sumPoint() {
     let point = 0;
-    for(let i = 0; i < this.#results.length; i++) {
-      point += this.#results[i].getPoint();
+    for(let i = 0; i < this.#points.length; i++) {
+      point += this.#points[i].getPoint();
     }
     return point;
   }
@@ -49,7 +50,7 @@ class PointCalculator {
   // pointの平均
   avePoint() {
     let point = this.sumPoint();
-    let i = this.#results.length;
+    let i = this.#points.length;
     return point / i ;
   }
 
@@ -58,25 +59,25 @@ class PointCalculator {
     let point = 0;
     let max = 0;
     let name = null;
-    for (let i = 0; i < this.#results.length; i++) {
-      point = this.#results[i].getPoint();
+    for (let i = 0; i < this.#points.length; i++) {
+      point = this.#points[i].getPoint();
       if (max < point) {
         max = point;
-        name = this.#results[i].getName();
+        name = this.#points[i].getName();
       }
     }
     return name;
   }
 }
 
-let results = [{name: '鈴木', point: 80}, {name: '田中', point: 92}, {name: '佐藤', point: 75}];
-let pointCalculator = PointCalculator.valueOf(results);
+let points = [{name: '鈴木', point: 80}, {name: '田中', point: 92}, {name: '佐藤', point: 75}];
+let pointCalculator = PointCalculator.valueOf(points);
 
 console.log(pointCalculator.sumPoint()); // 合計 247
 console.log(pointCalculator.avePoint()); // 平均 82.33…
 console.log(pointCalculator.maxPointName()); // 田中
 
-pointCalculator.addResult(new ImmutablePoint('阿部', 95));
+pointCalculator.addResult(new Point('阿部', 95));
 
 console.log(pointCalculator.sumPoint()); // 合計 342
 console.log(pointCalculator.avePoint()); // 平均 85.5
